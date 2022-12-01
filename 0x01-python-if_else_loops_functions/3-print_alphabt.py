cd81c52a4318c7bcfa84868088cd4738d13238b3
Igoni Obebharo <obebharoi@gmail.com>
2022-11-30 20:59:24 -0800

    Yes

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
-#Author - Igoni Obebharo
-for letter in range (97,123):
-	if chr(letter != 'q' and chr(letter) != 'e':
-		print("{}".format(chr(letter)), end="")
+#!/usr/bin/python3
+# Author - Igoni Obebharo Talbot 
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
